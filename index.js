4b40ed1f99197e8649b159197859fb2c4adda6e3
Kiran Tallapragada <kiranmobility@gmail.com>
2017-12-07 09:11:16 +0530

    express bootstarp-01

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var _  = require('lodash')
+var engines = require('consolidate')
+var  users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function (err,data) {
+    if(err) throw  err;
+    JSON.parse(data).forEach(function (user) {
+        user.name.full = _.startCase(user.name.first+' '+user.name.last)
+        users.push(user)
+    })
+})
+
+app.engine('hbs',engines.handlebars)
+app.set('views','./views')
+app.set('view engine','hbs')
+app.use('/profile', express.static('images'))
+
+app.get('/',function (req,res) {
+    res.render('index',{users: users})
+})
+
+app.get('/:username',function (req,res) {
+    res.render('user',{username: req.params.username})
+})
+
+var server = app.listen(3000,function () {
+    console.log('Sever running at http://localhost:'+server.address().port)
+})
